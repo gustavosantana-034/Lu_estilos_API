e025d72705465dc43d9bbeda07abd5e4dd407e77
Darkpwd <natanyuri1010@gmail.com>
2025-05-23 10:14:38 -0300

    feat: changes done!

--- scripts/check_commit.sh
+++ scripts/check_commit.sh
@@ -4,8 +4,8 @@ COMMIT_MSG_FILE=$1
 REGEX="^(feat|fix|docs|style|refactor|test|chore): .+"
 
 if ! grep -qE "$REGEX" "$COMMIT_MSG_FILE"; then
-  echo "❌ Commit inválido!"
-  echo "Use o formato: 'tipo: descrição'"
-  echo "Ex: 'feat: adiciona rota de login'"
+  echo "❌ Invalid commit message format."
+  echo "Please use the format: 'type: description'"
+  echo "Example: 'feat: add login route'"
   exit 1
 fi
